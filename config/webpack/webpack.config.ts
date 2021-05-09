633e8cdd0a97c6c5dd0e1af65a9d32b79bd308c8
kitce <kitce.dev@gmail.com>
2021-05-09 23:35:40 +0800

    Use `namespace` as the entry name

--- config/webpack/webpack.config.ts
+++ config/webpack/webpack.config.ts
@@ -2,11 +2,11 @@ import webpack from 'webpack';
 import scss from './rules/scss';
 import typescript from './rules/typescript';
 import userscript from './plugins/userscript';
-import { name } from '../../package.json';
+import { namespace } from '../../package.json';
 
 const config: webpack.Configuration = {
   entry: {
-    [name]: './src/main.ts'
+    [namespace]: './src/main.ts'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
