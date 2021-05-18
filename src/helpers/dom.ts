8a8f40f1e71fb7df842d8f1a8c75c8fb3927856f
kitce <kitce.dev@gmail.com>
2021-05-19 04:01:36 +0800

    Remove unnecessary `createStylesheet()`

--- src/helpers/dom.ts
+++ src/helpers/dom.ts
@@ -1,10 +1,3 @@
-export const createStylesheet = (stylesheet: string) => {
-  const style = document.createElement('style');
-  style.setAttribute('type', 'text/css');
-  style.innerHTML = stylesheet;
-  return style;
-};
-
 export const insertAfter = (newChild: Node, referenceChild: Node) => {
   return referenceChild.parentNode?.insertBefore(newChild, referenceChild.nextSibling);
 };
