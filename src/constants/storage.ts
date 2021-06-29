bc3b5a4646c38e42164630f88ad912828d84f424
kitce <kitce.dev@gmail.com>
2021-06-30 00:52:59 +0800

    Rename `DEPRECATED_LOCAL_STORAGE_KEYS` to `DEPRECATED_LOCAL_STORAGE_DATA_KEYS`

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,5 +1,6 @@
 export const DATA_KEY = 'data';
+export const DRAFTS_KEY = 'drafts';
 
-export const DEPRECATED_LOCAL_STORAGE_KEYS = [
+export const DEPRECATED_LOCAL_STORAGE_DATA_KEYS = [
   'LIHKG-Label-Users-data'
 ];
--- src/storage.ts
+++ src/storage.ts
@@ -1,9 +1,9 @@
-import { DATA_KEY, DEPRECATED_LOCAL_STORAGE_KEYS } from './constants/storage';
+import { DATA_KEY, DEPRECATED_LOCAL_STORAGE_DATA_KEYS } from './constants/storage';
 import Storage from './models/Storage';
 
 const keys = [
   DATA_KEY,
-  ...DEPRECATED_LOCAL_STORAGE_KEYS
+  ...DEPRECATED_LOCAL_STORAGE_DATA_KEYS
 ];
 
 const storage = new Storage(keys);
