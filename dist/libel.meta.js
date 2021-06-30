12a6da09751cfd8edc16d65ce84caecae935ecd9
kitce <kitce.dev@gmail.com>
2021-07-01 01:48:55 +0800

    1.0.8

--- dist/libel.meta.js
+++ dist/libel.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Libel
-// @version     1.0.7
+// @version     1.0.8
 // @author      kitce <kitce.dev@gmail.com>
 // @description Label users on LIHKG
 // @homepage    https://kitce.github.io/libel
