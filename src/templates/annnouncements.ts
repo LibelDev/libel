459e521abb7415ceb7a133ffe0ea25a820de418a
kitce <kitce.dev@gmail.com>
2021-07-01 02:55:45 +0800

    Refactor `newVersion` announcement template

--- src/templates/annnouncements.ts
+++ src/templates/annnouncements.ts
@@ -1,3 +1,3 @@
-import { displayName } from '../../package.json';
+import { displayName, homepage } from '../../package.json';
 
-export const newVersion = `【${displayName}】 <a href="https://kitce.github.io/libel/dist/libel.user.js" target="_blank">新版本<strong>{{newVersion}}</strong></a> 已推出（<a href="{{release.html_url}}" target="_blank">更新內容</a>）`;
+export const newVersion = `【${displayName}】 <a href="${homepage}/dist/libel.user.js" target="_blank">新版本<strong>{{newVersion}}</strong></a> 已推出（<a href="{{release.html_url}}" target="_blank">更新內容</a>）`;
