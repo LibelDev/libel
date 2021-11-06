70223069d35eea87f136c474aa65088255fd2f63
kitce <kitce.dev@gmail.com>
2021-11-06 22:20:33 +0800

    Fix Webpack config `publicPath`

--- config/webpack/webpack.config.base.ts
+++ config/webpack/webpack.config.base.ts
@@ -1,4 +1,5 @@
 import webpack from 'webpack';
+import { publicURL } from './../config';
 import environment from './plugins/environment';
 import { resource, source } from './rules/asset';
 import scss from './rules/scss';
@@ -7,6 +8,9 @@ import typescript from './rules/typescript';
 const config: webpack.Configuration = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    publicPath: `${publicURL}/`
   },
   module: {
     rules: [
