1fa312cddf0c4f5b4d9caa872f7080b8d005bac8
kitce <kitce.dev@gmail.com>
2022-02-26 04:14:46 +0800

    refactor(main): only import stylesheets in main app

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import * as egg from './constants/egg';
 import { isMainApp, isOffline } from './helpers/app';
 import { appendScript } from './helpers/dom';
-import './stylesheets/lihkg.scss';
-import './stylesheets/main.scss';
+// import './stylesheets/lihkg.scss';
+// import './stylesheets/main.scss';
 
 if (!isOffline()) {
   (async () => {
     appendScript(egg.src);
     if (isMainApp()) {
+      require('./stylesheets/lihkg.scss');
+      require('./stylesheets/main.scss');
       const { default: app } = await import('./app');
       const { default: storage } = await import('./storage');
       await storage.ready();
