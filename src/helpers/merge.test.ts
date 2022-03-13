05a098969c600eb039778a48f299d1d3db8fe56c
kitce <kitce.dev@gmail.com>
2022-03-14 02:58:08 +0800

    test(merge): add test file

---
+++ src/helpers/merge.test.ts
@@ -0,0 +1,220 @@
+import { IDataSet } from '../models/DataSet';
+import { ISerializedSubscription } from '../models/Subscription';
+import { mergeDataSet, mergeSubscriptions } from './merge';
+
+describe('mergeDataSet', () => {
+  it('should have 1 user, 1 label', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 2B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, false);
+    expect(Object.keys(merged.data).length).toBe(1);
+    expect(merged.data['1']!.length).toBe(1);
+    expect(merged.data['1']![0]).toEqual(dataSetB.data['1']![0]);
+  });
+
+  it('should have 1 user, 2 labels', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '1': [
+          { id: '2', text: 'Label 2B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, false);
+    expect(Object.keys(merged.data).length).toBe(1);
+    expect(merged.data['1']!.length).toBe(2);
+    expect(merged.data['1']![0]).toBe(dataSetA.data['1']![0]);
+    expect(merged.data['1']![1]).toBe(dataSetB.data['1']![0]);
+  });
+
+  it('should have 2 users, 2 labels', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '2': [
+          { id: '1', text: 'Label 1B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, false);
+    expect(Object.keys(merged.data).length).toBe(2);
+    expect(merged.data['1']!.length).toBe(1);
+    expect(merged.data['1']![0]).toBe(dataSetA.data['1']![0]);
+    expect(merged.data['2']!.length).toBe(1);
+    expect(merged.data['2']![0]).toBe(dataSetB.data['2']![0]);
+  });
+
+  it('should have 1 user, 1 label', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '1': [
+          { id: '2', text: 'Label 2B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, true);
+    expect(Object.keys(merged.data).length).toBe(1);
+    expect(merged.data['1']!.length).toBe(1);
+    expect(merged.data['1']![0]).toBe(dataSetB.data['1']![0]);
+  });
+
+  it('should have 1 user, 2 labels', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1B' },
+          { id: '2', text: 'Label 2B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, true);
+    expect(Object.keys(merged.data).length).toBe(1);
+    expect(merged.data['1']!.length).toBe(2);
+    expect(merged.data['1']![0]).toEqual(dataSetB.data['1']![0]);
+    expect(merged.data['1']![1]).toBe(dataSetB.data['1']![1]);
+  });
+
+  it('should have 1 user, 2 labels', () => {
+    const dataSetA: IDataSet = {
+      data: {
+        '1': [
+          { id: '1', text: 'Label 1A' }
+        ]
+      }
+    };
+    const dataSetB: IDataSet = {
+      data: {
+        '2': [
+          { id: '1', text: 'Label 1B' },
+          { id: '2', text: 'Label 2B' }
+        ]
+      }
+    };
+    const merged = mergeDataSet(dataSetA, dataSetB, true);
+    expect(Object.keys(merged.data).length).toBe(1);
+    expect(merged.data['2']!.length).toBe(2);
+    expect(merged.data['2']![0]).toBe(dataSetB.data['2']![0]);
+    expect(merged.data['2']![1]).toBe(dataSetB.data['2']![1]);
+  });
+});
+
+describe('mergeSubscriptions', () => {
+  it('should have 1 subscription', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_1', enabled: false }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, false);
+    expect(merged.length).toBe(1);
+    expect(merged[0]).toEqual(subscriptionsB[0]);
+  });
+
+  it('should have 2 subscriptions', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_2', enabled: false }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, false);
+    expect(merged.length).toBe(2);
+    expect(merged[0]).toBe(subscriptionsA[0]);
+    expect(merged[1]).toBe(subscriptionsB[0]);
+  });
+
+  it('should have 2 subscriptions', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_1', enabled: false },
+      { name: 'C', url: 'URL_2', enabled: true }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, false);
+    expect(merged.length).toBe(2);
+    expect(merged[0]).toEqual(subscriptionsB[0]);
+    expect(merged[1]).toBe(subscriptionsB[1]);
+  });
+
+  it('should have 3 subscriptions', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_2', enabled: false },
+      { name: 'C', url: 'URL_3', enabled: true }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, false);
+    expect(merged.length).toBe(3);
+    expect(merged[0]).toBe(subscriptionsA[0]);
+    expect(merged[1]).toBe(subscriptionsB[0]);
+    expect(merged[2]).toBe(subscriptionsB[1]);
+  });
+
+  it('should have 1 subscription', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_2', enabled: false }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, true);
+    expect(merged.length).toBe(1);
+    expect(merged[0]).toBe(subscriptionsB[0]);
+  });
+
+  it('should have 2 subscriptions', () => {
+    const subscriptionsA: ISerializedSubscription[] = [
+      { name: 'A', url: 'URL_1', enabled: true }
+    ];
+    const subscriptionsB: ISerializedSubscription[] = [
+      { name: 'B', url: 'URL_2', enabled: false },
+      { name: 'C', url: 'URL_3', enabled: true }
+    ];
+    const merged = mergeSubscriptions(subscriptionsA, subscriptionsB, true);
+    expect(merged.length).toBe(2);
+    expect(merged[0]).toBe(subscriptionsB[0]);
+    expect(merged[1]).toBe(subscriptionsB[1]);
+  });
+});
