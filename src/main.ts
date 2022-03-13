25dff3948419c1a8c7037a68b861f9d43f2897cc
kitce <kitce.dev@gmail.com>
2022-03-14 02:38:51 +0800

    fix(main): app start performance (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as egg from './constants/egg';
 import { isMainApp, isOffline } from './helpers/app';
 import { appendScript } from './helpers/dom';
-// import './stylesheets/lihkg.scss';
-// import './stylesheets/main.scss';
 
 if (!isOffline()) {
   (async () => {
@@ -11,9 +9,7 @@ if (!isOffline()) {
       require('./stylesheets/lihkg.scss');
       require('./stylesheets/main.scss');
       const { default: app } = await import('./app');
-      const { default: storage } = await import('./storage');
-      await storage.ready();
-      await app.start();
+      app.start();
       require('./sync');
       require('./ga');
     }
