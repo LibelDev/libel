778168dd9737212eee78044d209cf002cdcf4035
kitce <kitce.dev@gmail.com>
2022-03-20 04:40:09 +0800

    build(postcss): add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
-const preset = require('postcss-preset-env');
 const tailwindcss = require('tailwindcss');
+const preset = require('postcss-preset-env');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     tailwindcss(),
-    preset()
+    preset(),
+    cssnano()
   ]
 };
