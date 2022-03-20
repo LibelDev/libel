74727a69116523a853cfc67b50fec74c81288e6e
kitce <kitce.dev@gmail.com>
2022-03-20 22:38:14 +0800

    fix(label-item): remove `color`

--- src/components/LabelItem/LabelItem.tsx
+++ src/components/LabelItem/LabelItem.tsx
@@ -1,23 +1,13 @@
 import classNames from 'classnames';
-import invert from 'invert-color';
-import React, { useMemo } from 'react';
+import React from 'react';
 import styles from './LabelItem.module.scss';
 
-interface IProps {
-  color?: string;
-}
+interface IProps { }
 
 type TProps = IProps & React.ComponentPropsWithoutRef<'div'>;
 
 const LabelItem = React.forwardRef<HTMLDivElement, TProps>((props, ref) => {
-  const { className, children, color, ...otherProps } = props;
-
-  const style: Partial<React.CSSProperties> | undefined = useMemo(() => (color ? {
-    backgroundColor: color,
-    borderColor: color,
-    color: color && invert(color, true)
-  } : undefined), [color]);
-
+  const { className, children, ...otherProps } = props;
   return (
     <div
       ref={ref}
@@ -27,7 +17,6 @@ const LabelItem = React.forwardRef<HTMLDivElement, TProps>((props, ref) => {
           styles.labelItem
         )
       }
-      style={style}
       {...otherProps}
     >
       <div className={styles.inner}>
