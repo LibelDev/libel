e9300f7ddd28c07b8d461d39d0c59236120565b0
kitce <kitce.dev@gmail.com>
2022-03-30 03:17:20 +0800

    feat(components): add prop `icon` to `TextInput`

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,18 +1,26 @@
 import classNames from 'classnames';
 import React from 'react';
 import BaseInput, { TProps as TBaseInputProps } from '../BaseInput/BaseInput';
+import Icon from '../Icon/Icon';
+import { IconName } from '../Icon/types';
 import styles from './TextInput.module.scss';
 
 interface IProps {
+  icon?: IconName;
   error?: React.ReactNode;
 }
 
-type TProps = IProps & TBaseInputProps;
+export type TProps = IProps & TBaseInputProps;
 
 const TextInput: React.FunctionComponent<TProps> = (props) => {
-  const { className, error, ...otherProps } = props;
+  const { className, icon, error, ...otherProps } = props;
   return (
     <div className={classNames(className, styles.textInput)}>
+      {
+        icon && (
+          <Icon className={styles.icon} icon={icon} />
+        )
+      }
       <BaseInput
         {...otherProps}
         type="text"
@@ -20,6 +28,7 @@ const TextInput: React.FunctionComponent<TProps> = (props) => {
           classNames(
             styles.input,
             {
+              [styles.hasIcon]: !!icon,
               [styles.error]: !!error
             }
           )
