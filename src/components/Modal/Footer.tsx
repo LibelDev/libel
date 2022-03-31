bcb969419814326036974904dcf5b84ec60db885
kitce <kitce.dev@gmail.com>
2022-04-01 03:52:29 +0800

    fix(modal): issue of `Footer` being shrinked

--- src/components/Modal/Footer.tsx
+++ src/components/Modal/Footer.tsx
@@ -1,16 +1,23 @@
+import classNames from 'classnames';
 import React from 'react';
 import styles from './Footer.module.scss';
 
 interface IProps { }
 
-type TProps = IProps & React.ComponentPropsWithoutRef<'div'>
+type TProps = IProps & React.ComponentPropsWithoutRef<'div'>;
 
 const Footer: React.FunctionComponent<TProps> = (props) => {
-  const { children } = props;
+  const { className, ...otherProps } = props;
   return (
-    <div className={styles.footer}>
-      {children}
-    </div>
+    <div
+      {...otherProps}
+      className={
+        classNames(
+          className,
+          styles.footer
+        )
+      }
+    />
   );
 };
 
