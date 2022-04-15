d3686a12a4857577a4ce4f7fae80af14a68bd79b
kitce <kitce.dev@gmail.com>
2022-04-16 03:28:12 +0800

    fix(hooks): incorrect ref object

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,12 +1,12 @@
 import { useCallback, useRef } from 'react';
 
 type TUseFocusResult<T> = [
-  React.MutableRefObject<T | null>,
+  React.RefObject<T>,
   (options?: FocusOptions) => void
 ];
 
 const useFocus = <T extends HTMLElement> (): TUseFocusResult<T> => {
-  const ref = useRef<T | null>(null);
+  const ref = useRef<T>(null);
   const focus = useCallback((options?: FocusOptions) => {
     ref.current?.focus(options);
   }, [ref]);
