efc92f3a5c65418c17cd41e66139cdb826a66fd3
kitce <kitce.dev@gmail.com>
2022-04-17 05:15:54 +0800

    refactor(egg): revise imports

--- src/models/EasterEgg.ts
+++ src/models/EasterEgg.ts
@@ -1,4 +1,4 @@
-import * as config from '../../config/config';
+import { debugEgg } from '../../config/config';
 
 type THatch = () => void;
 
@@ -15,7 +15,7 @@ class EasterEgg {
 
   hatch () {
     const { enabled } = this;
-    if (config.debugEgg || enabled) {
+    if (debugEgg || enabled) {
       this[_hatch]();
     }
   }
