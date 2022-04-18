906b1b257c535ffc48de882cd6d7b81af1af20f3
kitce <kitce.dev@gmail.com>
2022-04-18 20:53:27 +0800

    bump: version 3.4.1 → 3.5.0

--- dist/libel.meta.js
+++ dist/libel.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name        Libel
-// @version     3.4.1
+// @version     3.5.0
 // @author      kitce <kitce.dev@gmail.com>
 // @description Label users on LIHKG
 // @homepage    https://kitce.github.io/libel
 // @namespace   https://github.com/kitce/libel
 // @include     https://lihkg.com/*
+// @exclude     https://lihkg.com/me/*
+// @exclude     https://lihkg.com/stickers
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
