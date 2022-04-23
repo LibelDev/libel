73d50138c59bc5e0cbdb1f96ee27259fcadf8c27
kitce <kitce.dev@gmail.com>
2022-04-24 03:41:32 +0800

    feat(apis/lihkg): add `fetchBlockedUser()`

---
+++ src/apis/lihkg.ts
@@ -0,0 +1,10 @@
+import type { APIv2 } from '../types/lihkg';
+
+const baseURL = 'https://lihkg.com/api_v2';
+
+export const fetchBlockedUser = async () => {
+  const url = `${baseURL}/me/blocked-user`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json as APIv2.IBlockedUserResponseBody;
+};
