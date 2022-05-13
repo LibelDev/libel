1cc70db84d8b4426594182335e78ecaffeb31207
kitce <kitce.dev@gmail.com>
2022-05-14 02:25:25 +0800

    refactor(apis): reuse `RequestMethod`

--- src/apis/nacx.ts
+++ src/apis/nacx.ts
@@ -1,3 +1,4 @@
+import { RequestMethod } from '../types/http';
 import type { IUploadResponse } from '../types/nacx';
 
 const baseURL = 'https://api.na.cx';
@@ -6,7 +7,7 @@ export const uploadImage = async (image: Blob) => {
   const url = `${baseURL}/upload`;
   const body = new FormData();
   body.append('image', image);
-  const init: RequestInit = { method: 'POST', body };
+  const init: RequestInit = { method: RequestMethod.POST, body };
   const response = await fetch(url, init);
   const json = await response.json();
   return json as IUploadResponse;
